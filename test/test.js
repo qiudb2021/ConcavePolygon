99058c92aca954d3f59277baaa74a0f5fab0292b
qiudb2021 <qiudb2022@outlook.com>
2022-01-27 20:36:30 +0800

    add draw polygon

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const {graphical, Rectangle} = require("graphical");
+const {graphical, Rectangle, Polygon} = require("graphical");
 
 graphical(8001);
 
@@ -6,3 +6,8 @@ var rectangle = new Rectangle();
 rectangle.setPos(0, 0);
 rectangle.setColor('blue');
 rectangle.setSize(20, 20);
+
+let plist = [{x:100, y:100}, {x:200, y:100}, {x:250,y:150}, {x:200, y:200}, {x:100, y:200}]
+let polygon = new Polygon();
+polygon.setPosList(plist);
+polygon.setColor("gray")
