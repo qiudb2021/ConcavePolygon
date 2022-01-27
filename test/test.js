9a20e5a8cab8d96edabfc791e6c960236ad03031
qiudb2021 <qiudb2022@outlook.com>
2022-01-27 20:24:22 +0800

    npm package graphical

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const {graphical, Rectangle} = require("graphical");
+
+graphical(8001);
+
+var rectangle = new Rectangle();
+rectangle.setPos(0, 0);
+rectangle.setColor('blue');
+rectangle.setSize(20, 20);
