69a45152118f8596ba34a4d4955b7fca8cfbcc16
qiudb2021 <qiudb2022@outlook.com>
2022-01-29 15:20:59 +0800

    finished

--- polygon2Convex2.js
+++ polygon2Convex2.js
@@ -13,19 +13,72 @@ const util = require("./util");
  * @param {{x: number, y: number}[][]} results 分割后的多边形
  */
 function divideConcavePoly(poly, results) {
+    if (_divideConcavePoly(poly, results)) return true;
+    for (let i = 0; i < 2; i++) {
+        poly.unshift(poly.pop());
+        if (_divideConcavePoly(poly, results)) return true;
+    }
     
-    console.log(poly)
+    console.log("切割失败")
+    return false;
+}
 
+
+/**
+ * @description 判断多边形是否是凹多边形
+ * @param {{x: number, y: number}[]} poly 
+ * @returns {boolean}
+ */
+function isConcavePoly(poly) {
+    if (!poly.length || poly.length < 4) return false;
+
+    // 遍历所有顶点
+    let p0 = poly[0], p1 = poly[1], p2 = poly[2];
+
+    let preValue = crossProduct({x: p1.x-p0.x, y: p1.y-p0.y}, {x: p2.x-p1.x, y: p2.y-p1.y}) >= 0 ? 1 : -1;
+    for (let i = 1; i < poly.length; i++) {
+        p0 = poly[i];
+        p1 = poly[(i+1)%poly.length];
+        p2 = poly[(i+2)%poly.length];
+        
+        let curValue = crossProduct({x: p1.x-p0.x, y: p1.y-p0.y}, {x: p2.x-p1.x, y: p2.y-p1.y}) >= 0 ? 1 : -1
+        if (curValue * preValue < 0) {
+            return true;
+        }
+    }
+}
+/**
+ * 
+ * @param {{x:number, y: number}[]} poly 多边形顶点列表
+ * @param {{x: number, y: number}[][]} results 切割后的多边形
+ */
+function _divideConcavePoly(poly, results) {
+    poly = JSON.parse(JSON.stringify(poly))
+    if (!poly.length) return true;
+    if (poly.length < 4) {
+        console.log("result is %j", results)
+        results.push(poly);
+        return true;
+    }
+
+    if (!isConcavePoly(poly)) {
+        results.push(poly);
+        return true;
+    }
+
+    console.log(poly)
     let len = poly.length;
     let breakFlag = false;
-
+    
     // 用poly1和poly2分别保存分割出来的两个多边形; poly1初始化为原多边形的深拷贝, poly2为空
     let poly1 = JSON.parse(JSON.stringify(poly));
     let poly2;
-
+    
     // 遍历所有顶点
     let i, j;
     for (i = 0; i < len - 2; i++) {
+        // if (i!=3) continue;
+        // if (i >0) continue
         // 当前顶点
         let p0 = poly[i];
         // 下个顶点
@@ -33,19 +86,23 @@ function divideConcavePoly(poly, results) {
         // 下下个顶点
         let p2 = poly[i+2];
         
+        if (i == len - 2-1) {
+            console.log(p0, p1, p2)
+        }
         // 将当前顶点p0和下个顶点p1的连线向量作为x轴
-        let vAxis = {x: p1.x-p0.x, y: p1.y-p2.y};
-        util.drawLine(p0, p1, "red", 5);
-
+        let vAxis = {x: p1.x-p0.x, y: p1.y-p0.y};
         // 当前顶点p0和下下个顶点p2的连线向量
+        // util.drawLine(p0, p1, "red", 5);
         let v = {x: p2.x-p0.x, y: p2.y-p0.y}
-        util.drawLine(p2, p0, "black", 5);
+        // util.drawLine(p0, p2, "yellow")
         if (crossProduct(vAxis, v) < 0) {
-            console.log("p0p2在p0p1的下方");
+            // util.drawLine(p2, p0, "black", 2);
+            // console.log("p0p2在p0p1的下方");
             for (j = i + 3; j < len; j++) {
                 v = {x: poly[j].x - p0.x, y: poly[j].y - p0.y};
                 if (crossProduct(vAxis, v) > 0) {
                     console.log("分割点%j", poly[j]);
+                    // util.drawLine(p0, poly[j], "blue", 5)
                     breakFlag = true;
                     break;
                 }
@@ -54,19 +111,32 @@ function divideConcavePoly(poly, results) {
 
         if (breakFlag) break;
     }
+
+    // 没有分割成功 
+    if (!breakFlag) return false;
+
     // 此时分割多边形的两个点分别为poly[i+1], poly[j];
     let dp1 = poly[i+1];
     let dp2 = poly[j];
-    console.log("分割多边形的两个点分别为%j和%j", dp1, dp2)
+    // console.log("分割多边形的两个点分别为%j和%j", dp1, dp2)
+    util.drawLine(dp1, dp2, "red", 5)
+
+    // util.drawCircle(dp1, 5, "red");
+    // util.drawCircle(dp2, 5, "red");
+
 
     // 从原来的多边形按照分割点分割出另一个子多边形也保存到poly2
     poly2 = poly.splice(i+2, j-(i+2));
     poly2.unshift(dp1);
     poly2.push(dp2);
-    util.drawPolySolid(poly, "yellow");
-    util.drawPolySolid(poly2, "green")
-    console.log(poly);
-    console.log(poly2);
+
+    // util.drawPolySolid(poly, "yellow");
+    // util.drawPolySolid(poly2, "green")
+    // console.log(poly);
+    // console.log(poly2);
+    divideConcavePoly(poly, results);
+    divideConcavePoly(poly2, results);
+    return true;
 }
 
 /**
@@ -80,6 +150,7 @@ function sub(p1, p2) {
 }
 
 function crossProduct(v1, v2) {
+    console.log(v1.x*v2.y - v2.x*v1.y)
     return v1.x*v2.y - v2.x*v1.y
 }
 
--- test/test.js
+++ test/test.js
@@ -7,26 +7,56 @@ graphical(8001);
 
 let polys = [ 
 
-    [
-        {x:100, y:100},
-        {x:260, y:180},
-        {x:400, y:100},
-        {x:400, y:340},
-        {x:100, y:340}
-    ], 
+    // [
+    //     {x:100, y:100},
+    //     {x:260, y:180},
+    //     {x:400, y:100},
+    //     {x:400, y:340},
+    //     {x:100, y:340}
+    // ], 
 
+    [
+        {x: 200, y: 100},
+        {x: 240, y: 100},
+        {x: 240, y: 400},
+        {x: 380, y: 400},
+        {x: 380, y: 440},
+        {x: 100, y: 440},
+        {x: 100, y: 400},
+        {x: 200, y: 400}
+    ],
+
+    // [ 
+    //     { x: 200, y: 100 },
+    //     { x: 240, y: 100 },
+    //     { x: 240, y: 400 },
+    //     { x: 100, y: 440 },
+    //     { x: 100, y: 400 },
+    //     { x: 200, y: 400 } 
+    // ],
+    // [ 
+    //     { x: 200, y: 400 },
+    //     { x: 200, y: 100 },
+    //     { x: 240, y: 100 },
+    //     { x: 240, y: 400 },
+    //     { x: 100, y: 440 } 
+    // ]
 ]
 
+let results = [];
 polys.forEach(poly => {
         
     console.log("%o 是否是凹多边形: %j", poly, polygon2Convex.isConcavePolygon(poly))
     
     // polygon2Convex.divideConcavePolygon(JSON.parse(JSON.stringify(poly)));
-    polygon2Convex2.divideConcavePoly(poly)
+    
+    polygon2Convex2.divideConcavePoly(poly, results)
     util.drawPolyLine(poly, "gray", 2);
     poly.forEach((p, index) => {
-        util.drawText(p, "p"+index+"("+p.x+","+p.y+")")
+        util.drawText(p, "p"+index,"black")
     })
+
+    console.log("最终切割结果：%o", results)
 })
 
 util.drawGrid(util.findMinXY(polys), util.findMaxXY(polys), "gray")
